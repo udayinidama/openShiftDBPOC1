9afede3071ca88edce8fbd4a0273f72d6d632b7d
udayinidama <61378290+udayinidama@users.noreply.github.com>
2020-02-24 17:45:14 +0400

    initial commit

---
+++ demoDB1/src/main/java/com/example/demoDB1/DemoDb1Application.java
@@ -0,0 +1,13 @@
+package com.example.demoDB1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDb1Application.class, args);
+	}
+
+}
