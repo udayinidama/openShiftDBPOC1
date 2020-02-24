65725509b611c005fd5fd723160b492a11a7072c
udayinidama <61378290+udayinidama@users.noreply.github.com>
2020-02-24 20:14:02 +0400

    commit

---
+++ demoDB1/src/main/java/com/example/demoDB1/Test.java
@@ -0,0 +1,13 @@
+package com.example.demoDB1;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Test {
+
+	
+	private int a;
+}
---
+++ demoDB1/src/main/java/com/example/demoDB1/TestRepository.java
@@ -0,0 +1,8 @@
+package com.example.demoDB1;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TestRepository extends CrudRepository<Test, String> {
+
+
+}
